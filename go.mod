998ebab47a707c2578748dce66beaa033efef094
qydysky <qydysky@foxmail.com>
2020-07-27 05:32:53 +0800

    7

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/qydysky/checkFile
 
 go 1.14
 
-require github.com/qydysky/part v0.0.0-20200726134617-30f45753b067
+require github.com/qydysky/part v0.0.0-20200726211655-392687193d39
 
 //replace github.com/qydysky/part => ../part
--- main.go
+++ main.go
@@ -8,27 +8,35 @@ import (
 
 var (
 	checkfile = flag.String("o", "", "output")
+	root = flag.String("r", "", "root")
 	checkDir = flag.String("i", "", "input")
 	SplitString = flag.String("s", "\n", "SplitString")
+	md5 = flag.Bool("m", true, "use md5")
 )
 
 func main() {
 	flag.Parse()
 
 	if *checkfile == "" {
-		part.Logf().E("build:")
-		part.Logf().E("-i {checkDir} -o {checkfile} -s {SplitString}(default:\"\\n\")")
+		part.Logf().E("build {当存在以下必要参数时进行构建}")
+		part.Logf().E("-i {你想要创建关于哪个目录的列表}")
+		part.Logf().E("-o {列表输出的位置}")
+		part.Logf().E("-r {列表的相对根路径 默认./ 可选}")
+		part.Logf().E("-s {列表使用的分割符 默认\\n 可选}")
+		part.Logf().E("-m {对每个文件计算md5 默认true 可选}")
 		part.Logf().E("eg. go run main.go -i ./ -o t.txt")
 		part.Logf().E("")
-		part.Logf().E("check:")
-		part.Logf().E("-o {checkfile} -s {SplitString}(default:\"\\n\")")
+		part.Logf().E("check {当存在以下必要参数时进行检查}")
+		part.Logf().E("-o {列表的位置}")
+		part.Logf().E("-r {列表的相对根路径 默认./ 可选}")
+		part.Logf().E("-s {列表使用的分割符 默认\\n 可选}")
 		part.Logf().E("eg. go run main.go -o t.txt")
 		return
 	}
 
 	if *checkDir != "" {
-		part.Checkfile().Build(*checkfile,*checkDir,*SplitString)
+		part.Checkfile().Build(*checkfile,*root,*checkDir,*SplitString,*md5)
 	}
 	
-	part.Checkfile().CheckList(*checkfile,*SplitString)
+	part.Checkfile().CheckList(*checkfile,*root,*SplitString)
 }
